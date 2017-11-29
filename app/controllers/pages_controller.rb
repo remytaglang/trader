61c3f630f0ea4afe6bb464f872015ec923d38e9d
remy taglang <remy.taglang@gmail.com>
2017-11-29 01:38:02 +0100

    adding pourcentage

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,6 +5,8 @@ class PagesController < ApplicationController
     @orders = Order.all
     @total = 0
     @gain = 0
+    @initial = 0
+    @initialmaxence = 0
     @totalmaxence = 0
     @gainmaxence = 0
     @remy = @orders.where(owner: "Remy")
@@ -12,12 +14,14 @@ class PagesController < ApplicationController
       order.run_api
       @total = @total + order.total
       @gain = @gain + order.gain
+      @initial = @initial + order.totalentree
     end
     @maxence = @orders.where(owner: "Maxence")
     @maxence.each do |order|
       order.run_api
       @totalmaxence = @totalmaxence + order.total
       @gainmaxence = @gainmaxence + order.gain
+      @initialmaxence = @initialmaxence + order.totalentree
     end
     @timing = Time.new
   end
